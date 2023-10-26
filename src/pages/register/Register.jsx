08f175744dff077c341bd8372d24d74cbd8609b6
OurFather27 <148778934+OurFather27@users.noreply.github.com>
2023-10-26 09:48:42 -0400

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -26,7 +26,7 @@ export default function Register() {
         password: password.current.value,
       };
       try {
-        await axios.post("https://churchbackend.onrender.com/api/auth/register", user);
+        await axios.post("/auth/register", user);
         alert('succss')
         history.push("/login");
       } catch (err) {
